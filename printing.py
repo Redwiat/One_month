532ebd1ac8b135b746484f3083bd3503202c9ec0
Abdullah Al Redwan <61589799+Redwiat@users.noreply.github.com>
2021-08-09 14:16:27 +0600

    Add files via upload

---
+++ printing.py
@@ -0,0 +1,31 @@
+print("Winter is Comming")
+print("you know nothing", "Abdullah Al Redwan")
+print("Learning" "learning" "learning")
+print('''
+Learning, learning, learning
+When I was six years old, I kinda wet the bed
+My stepdad came in and nearly beat me to death
+All bruised and broken, I was lonely and scared
+My mom, she was working, there was nobody there
+My nana showed up, she was super protective''')
+
+# "since feeling is first" by e. e. cummings
+print("since feeling is first")
+print("who pays any attention")
+print("to the syntax of things")
+print("will never wholly kiss you;")
+print("wholly to be a fool")
+print("while Spring is in the world")
+print()
+print("my blood approves")
+#print("and kisses are a better fate"
+print("then wisdom")
+print("lady i swear by all flowers. Don't cry") # I love this line.
+print("—the best gesture of my brain is less then")
+print("you're eyelids' flutter which says")
+print()
+print("we are for each other: than")
+print("laugh, leaning back in my arms")
+print("for life's not a paragraph")
+print()
+print("and death i think is no parenthesis")
