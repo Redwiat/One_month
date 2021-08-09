1ea5c2be9e7ad1cb983641757d7a8d6cddc636cf
Abdullah Al Redwan <61589799+Redwiat@users.noreply.github.com>
2021-08-09 14:50:16 +0600

    Variables in Python
    Variables in Python

---
+++ variable.py
@@ -0,0 +1,11 @@
+# variables are name
+# there plain, lowercase words
+# examples: x, y, apple, phone_a_quail
+
+name = "Redwan"
+banana_fee = 500
+apple_fee = 120
+
+total = banana_fee + apple_fee
+
+print(name, "the total will be", total)
