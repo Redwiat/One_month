d751cb0176b63dff9510a45068033e44e86be894
Abdullah Al Redwan <61589799+Redwiat@users.noreply.github.com>
2021-08-09 15:10:54 +0600

    Python math functions
    Python is famous for it's ability to do amazing things with numbers and math.

---
+++ math.py
@@ -0,0 +1,24 @@
+#Python math functions 
+#Python is famous for it's ability to do amazing things with numbers and math.
+
+# + plus
+# - minus
+# / divide
+# * times
+# ** exponetiation
+# % mudulo
+# < less then
+# < greater then
+
+print("what is the answer to life, the universe, and everything?", int((467 + 318 - 31) *67 / 12) )
+
+
+#Let's practice some Python math
+
+print("Is it true that 86 * 7 > 312 * 941?")
+
+print(65 * 4 > 633 * 72)
+
+print("What is 759 * 63 =", 759 * 63)
+
+print("What is 328 * 39 =", 328 * 39)
