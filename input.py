d8085ba5a18530354cdb3e102a7dba32ea56c3e8
Abdullah Al Redwan <61589799+Redwiat@users.noreply.github.com>
2021-08-10 11:17:17 +0600

    Add files via upload

---
+++ input.py
@@ -0,0 +1,9 @@
+name = input("What's your name? ")
+age = input("How old are you? ")
+
+print(f"So, You are {name} and you are {age} years old ")
+
+country = input("Where you from? ")
+print(f"Ooh, You are from {country}")
+
+#run it
