a4518b987c4ae4145266f6daccae720ccff68faa
Abdullah Al Redwan <61589799+Redwiat@users.noreply.github.com>
2021-08-13 15:35:16 +0600

    Update if.py

--- if.py
+++ if.py
@@ -3,4 +3,18 @@ answer = input("Do you want to hear a joke? ")
 
 if answer == "Yes":
     print("I'm against picketing, but I don't know how to slow it.")
+   
+
+In if.py: 
+
+answer = input("Do you want to hear a joke? ")
+
+if answer == "Yes" or answer == "yes":
+    print("I'm against picketing, but I don't know how to show it.")
+    # Mitch Hedberg (RIP)
+elif answer == "No" or answer == "no" :
+    print("Fine.")
+else:
+    print("I don't understand.")
+    
     
