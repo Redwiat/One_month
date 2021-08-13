48b5d7d578ebdae0bfec582b39ead4cde71931bc
Abdullah Al Redwan <61589799+Redwiat@users.noreply.github.com>
2021-08-13 13:58:19 +0600

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -20,3 +20,34 @@ print(f"Your Bill is ${price:.2f} and your tip is ${tip_amt:.2f} ")
 
 print(f"Your total with tax is ${total:.2f}")
 
+
+# Write a script that takes a dollar amount and recommennds a tip (15%, 18% and 20%)
+# Get a name
+name = input("What is you're name? ")
+print(name)
+
+total = int(float(input("What is you're bill sub-total? ").replace('$', '')))
+print(total)
+
+tip_15 = total * 0.15
+tip_18 = total * 0.18
+tip_20 = total * 0.20
+
+print(f"15% is ${tip_15:.2f}")
+print(f"18% is ${tip_18:.2f}")
+print(f"20% is ${tip_20:.2f}")
+
+
+# Write a script that takes a dollar amount and recommennds a tip (15%, 18% and 20%)
+
+total = float(input("What is you're bill sub-total? ").strip('$'))
+
+tip_15 = total * 0.15
+tip_18 = total * 0.18
+tip_20 = total * 0.20
+
+print("Tip Suggestions:")
+print("----------------")
+print(f"15% is ${tip_15:.2f}")
+print(f"18% is ${tip_18:.2f}")
+print(f"20% is ${tip_20:.2f}")
